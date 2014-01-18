b018bead36f69baf4d5b1f1193e30dde0de35caa
Francisco José Marques Vieira <francisco.vieira@co.sapo.pt>
2014-01-18 18:12:30 +0000

    Use abjad Duration

--- random_music_generator.py
+++ random_music_generator.py
@@ -1,5 +1,5 @@
 import abjad
-from abjad import Rest, Note, Staff, Score, Measure, show
+from abjad import Rest, Note, Staff, Score, Measure, Duration as D, show
 from fractions import Fraction as F
 import random
 import copy
@@ -9,7 +9,7 @@ NOTES = ["c'", "d'", "e'", "f'", "g'", "a'", "b'",
 
 # List of pairs duration/probability. A duration with probability 5
 # compared to a duration with probability 3 will appear 5/3 times more.
-DURATIONS = [('1', F(1)), ('2', F(3)), ('4', F(5)), ('8', F(5))]
+DURATIONS = [(D(1), F(1)), (D(1, 2), F(3)), (D(1, 4), F(5)), (D(1, 8), F(5))]
 
 REST_PROBABILITY = F(1, 5)
 
@@ -31,13 +31,13 @@ def generate_random_measure(measure_size=4, notes=None, durations=None):
     measure_space_left = F(measure_size, 4)
     while measure_space_left > 0:
         durations = filter_duration_probabilities(durations, measure_space_left)
-        duration = pick_random_duration(durations)[0]
+        duration = pick_random_duration(durations)
         if random.random() < REST_PROBABILITY:
-            note = Rest('r' + duration)
+            note = Rest(duration)
         else:
-            note = Note(random.choice(NOTES) + duration)
+            note = Note(random.choice(NOTES), duration)
         notes.append(note)
-        measure_space_left -= F(1, int(duration))
+        measure_space_left -= duration
 
     assert measure_space_left == 0
     return Measure((measure_size, 4), notes)
@@ -47,7 +47,7 @@ def filter_duration_probabilities(durations, space_left):
     total_probability = 0
     filtered_durations = []
     for duration, probability in durations:
-        if F(1, int(duration)) <= space_left:
+        if duration <= space_left:
             filtered_durations.append((duration, probability))
             total_probability += probability
 
